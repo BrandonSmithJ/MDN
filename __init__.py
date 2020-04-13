67b9805486b11aae261bd82585f7e4d11472e017
Brandon Smith <Brandon.SmithJ@gmail.com>
2020-04-13 17:30:33 -0400

    Add requirements

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 from .__version__ import __version__
 from .product_estimation import image_estimates, apply_model, train_model
 from .meta import get_sensor_bands
