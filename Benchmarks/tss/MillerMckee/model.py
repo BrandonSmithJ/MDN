8918cef744a76247b2657411713aba0a0bcbc0aa
Brandon Smith <Brandon.SmithJ@gmail.com>
2020-05-19 16:12:00 -0400

    Add SOLID
    Former-commit-id: 84fad8088ec1413a183d20a4356a4108f5281b0e

---
+++ Benchmarks/tss/MillerMckee/model.py
@@ -0,0 +1,18 @@
+'''
+Copy this folder into a product directory, and rename it to be the algorithm name
+'''
+
+from ..utils import get_required, optimize
+
+# Define any optimizable parameters
+@optimize(['a', 'b'])
+def model(Rrs, wavelengths, *args, **kwargs):
+	required = [668]
+	tol = kwargs.get('tol', 5) # allowable difference from the required wavelengths
+	Rrs = get_required(Rrs, wavelengths, required, tol) # get values as a function: Rrs(443)
+
+	# Set default values for these parameters
+	a = kwargs.get('a', 1140.25)
+	b = kwargs.get('b', 1.91)
+
+	return a * Rrs(668) - b
--- benchmarks.py
+++ benchmarks.py
@@ -11,57 +11,86 @@ import numpy as np
 import pkgutil, warnings, sys, os
 
 
-def get_methods(wavelengths, sensor, product, **kwargs):
+def get_methods(wavelengths, sensor, product, debug=True, allow_opt=False, **kwargs):
+	''' Retrieve all benchmark functions from the appropriate product
+		directory. Import each function with "model" in the function
+		name, ensure any necessary parameters have a default value 
+		available, and test whether the function can be run with the
+		given wavelengths. A template folder for new algorithms is 
+		available in the Benchmarks directory.
+	'''
 	methods = {}
-	for (_, name, is_folder) in pkgutil.iter_modules([Path(__file__).parent.resolve().joinpath('Benchmarks',product)]):
+	sensor  = sensor.split('-')[0] # remove any extra data which is used by the MDN
+
+	# Iterate over all benchmark algorithm folders in the appropriate product directory
+	for (_, name, is_folder) in pkgutil.iter_modules([Path(__file__).parent.resolve().joinpath('Benchmarks', product)]):
 		if is_folder:
 			module   = Path(__file__).parent.stem
 			imported = import_module(f'{module}.Benchmarks.{product}.{name}.model')
 			for function in dir(imported):
 
-				if 'model' in function:
+				# Check all functions which have "model" in their name
+				if 'model' in function: 
 					model = getattr(imported, function)
-					if model.has_default:
-						try: 
-							out = model(np.ones((1, len(wavelengths))), wavelengths, sensor.split('-')[0], **kwargs)
-							if out is not None:
-								model_name          = getattr(model, 'model_name', name)
-								methods[model_name] = partial(model, sensor=sensor.split('-')[0])
-								methods[model_name].__name__ = model_name 
-						except Exception as e: 
-							# print(f'Exception for function {name}: {e}')
-							pass
-					#print(name,'is not valid for the given wavelengths')
-			else:
-				pass
-				# print(name,'requires optimization')
+					if hasattr(model, 'has_default'):
+						if model.has_default or allow_opt:
+
+							# Add a set of dummy parameters to check if wavelengths are valid for the method
+							if not model.has_default:
+								kwargs.update( dict(zip(model.opt_vars, [1]*len(model.opt_vars))) )
+
+							try: 
+								out = model(np.ones((1, len(wavelengths))), wavelengths, sensor, **kwargs)
+								if out is not None:
+									model_name          = getattr(model, 'model_name', name)
+									methods[model_name] = partial(model, sensor=sensor)
+									methods[model_name].__name__ = model_name 
+							except Exception as e: 
+								if debug:
+									print(f'Exception for function {name}: {e}')
+
+						# Models which don't have default parameters available can only be run through bench_opt	
+						elif debug:
+							print(name,'requires optimization')
 	return methods
 
 
-def bench_product(args, sensor, x_test, y_test, slices, silent=False, product='chl'):
-	assert(x_test.shape[1] <= len(get_sensor_bands(sensor))), (
-		'Too many features given as bands for %s:' % sensor, x_test.shape[1])
+def bench_product(args, sensor, x_test, y_test=None, slices=None, silent=True, product='chl', method=None):
+	assert(silent or y_test is not None), 'Must provide y values or set silent=True'
 
-	waves   = np.array(get_sensor_bands(sensor, args))
+	waves = get_sensor_bands(sensor, args)
+	assert(x_test.shape[1] <= len(waves)), f'Too many features given as bands for {sensor}: {x_test.shape[1]} vs {len(waves)}'
+
 	methods = get_methods(waves, sensor, product, tol=15)
+	assert(method is None or method in methods), f'Unknown algorithm "{method}". Options are: \n{list(methods.keys())}'
+
 	ests = []
 	lbls = []
-	for name, method in methods.items():
-		est_chl = method(x_test, waves, tol=15)
-		est_chl[~np.isfinite(est_chl)] = 0
-		est_chl[est_chl < 0] = 0
-		if not silent:
-			ins_val = y_test[:, slices[product]]
-			for i in range(ins_val.shape[1]):
-				print( performance(name.split('_')[0], ins_val[:, i], est_chl) )
-		ests.append(est_chl)
-		lbls.append(name)
+	for name, func in methods.items():
+		if method is None or name == method:
+			est_val = func(x_test, waves, tol=15)
+			
+			if product == 'chl':
+				est_val[~np.isfinite(est_val)] = 0
+				est_val[est_val < 0] = 0
+
+			if not silent:
+				curr_slice = slices
+				if slices is None:
+					assert(y_test.shape[1] == est_val.shape[1]), 'Ambiguous y data provided - need to give slices parameter.'
+					curr_slice = {product:slice(None)}
+
+				ins_val = y_test[:, curr_slice[product]]
+				for i in range(ins_val.shape[1]):
+					print( performance(name, ins_val[:, i], est_val) )
+			ests.append(est_val)
+			lbls.append(name)
 	return dict(zip(lbls, ests))
 	
 
 def bench_opt(args, sensor, x_train, x_test, y_train, y_test, slices, silent=False, product='chl'):
 	waves   = np.array(get_sensor_bands(sensor))
-	methods = get_methods(waves, sensor, product)
+	methods = get_methods(waves, sensor, product, allow_opt=True)
 
 	ests = []
 	lbls = []
@@ -78,7 +107,7 @@ def bench_opt(args, sensor, x_train, x_test, y_train, y_test, slices, silent=Fal
 	return dict(zip(lbls, ests))
 
 
-def bench_ml(args, sensor, x_train, y_train, x_test, y_test, slices={'chl':slice(0,1)}, silent=False, product='chl', x_other=None, 
+def bench_ml(args, sensor, x_train, y_train, x_test, y_test, slices=None, silent=False, product='chl', x_other=None, 
 			bagging=True, gridsearch=False, scale=True):
 	from sklearn.preprocessing import RobustScaler, QuantileTransformer, MinMaxScaler
 	from sklearn.model_selection import GridSearchCV
@@ -155,6 +184,10 @@ def bench_ml(args, sensor, x_train, y_train, x_test, y_test, slices={'chl':slice
 		x_test  = x_scaler.transform(x_test)
 		y_train = y_scaler.transform(y_train)
 
+	if slices is None:
+		assert(y_test.shape[1] == y_train.shape[1]), 'Ambiguous y data provided - need to give slices parameter.'
+		slices = {product:slice(None)}
+
 	with warnings.catch_warnings():
 		warnings.filterwarnings('ignore')
 		if gridsearch:
@@ -177,17 +210,17 @@ def bench_ml(args, sensor, x_train, y_train, x_test, y_test, slices={'chl':slice
 			if bagging:
 				model = BaggingRegressor(model, n_estimators=10, max_samples=0.75, bootstrap=False)
 			model.fit(x_train.copy(), y_train.copy().flatten())
-			est_chl = model.predict(x_test.copy())
+			est_val = model.predict(x_test.copy())
 
 			if scale:
-				est_chl = y_scaler.inverse_transform(est_chl[:,None]).flatten()	
+				est_val = y_scaler.inverse_transform(est_val[:,None]).flatten()	
 
 			if not silent:
 				ins_val = y_test[:, slices[product]]
 				for i in range(ins_val.shape[1]):
-					print( performance(method, ins_val[:, i], est_chl) )
+					print( performance(method, ins_val[:, i], est_val) )
 
-			ests.append(est_chl)
+			ests.append(est_val)
 			lbls.append(method)
 
 			if x_other is not None:
@@ -274,7 +307,7 @@ def bench_chl(*args, **kwargs):
 	return bench_product(*args, product='chl', **kwargs)
 
 
-def run_benchmarks(args, sensor, x_test, y_test, slices, silent=True, x_train=None, y_train=None, gridsearch=False):
+def run_benchmarks(args, sensor, x_test, y_test, slices, silent=True, x_train=None, y_train=None, gridsearch=False, with_ml=False):
 	benchmarks = {}
 	if len([k for k in slices if k[0] in ['a','b'] and '*' not in k and 'ad' not in k]):
 		benchmarks.update( bench_qaa(args, sensor, x_test, y_test, slices, silent) )
@@ -284,11 +317,14 @@ def run_benchmarks(args, sensor, x_test, y_test, slices, silent=True, x_train=No
 
 	if 'chl' in slices:
 		benchmarks.update( bench_chl(args, sensor, x_test, y_test, slices, silent) )
-		benchmarks.update( bench_ml(args, sensor, x_train, y_train, x_test, y_test, slices, silent, gridsearch=gridsearch) )
 		# benchmarks.update( bench_giop(sensor, x_test, y_test, slices, silent) )
+		if with_ml:
+			benchmarks.update( bench_ml(args, sensor, x_train, y_train, x_test, y_test, slices, silent, gridsearch=gridsearch) )
+
 
 	if 'tss' in slices:
-		benchmarks.update( bench_tss(args, sensor, x_test, y_test, slices, silent) )
+		# benchmarks.update( bench_tss(args, sensor, x_test, y_test, slices, silent) )
+		benchmarks.update( bench_product(args, sensor, x_test, y_test, slices, silent, product='tss') )
 
 	return benchmarks
 
