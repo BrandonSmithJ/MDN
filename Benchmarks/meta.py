e0a632ebe50636fafc65dd6148a6faf6d6712980
Brandon Smith <brandon.smithj@gmail.com>
2020-09-16 12:52:57 -0400

    Benchmark bug fixes and additions
    - Fixed SOLID
    - Added QAA
    - Added GIOP

---
+++ Benchmarks/meta.py
@@ -0,0 +1,16 @@
+# SeaWiFS coefficients
+h0 = -1.14590292783408 
+h1 = -1.36582826429176
+h2 = -0.469266027944581 
+
+# Gordon
+g0_Gordon = 0.0949
+g1_Gordon = 0.0794
+
+# Lee
+g0_Lee = 0.084
+g1_Lee = 0.17
+
+# QAA
+g0_QAA = 0.08945
+g1_QAA = 0.1247  
